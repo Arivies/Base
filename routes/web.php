ee41bcd826923a01e9c149c4c1144be87a373985
arivies <ari.ve08@gmail.com>
2021-10-17 22:41:51 -0700

    Instalado paquete Laravel-Permission

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 /*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
+})->name('dashboard');*/
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+    return view('admin.index');
 })->name('dashboard');
 
+
+
+/*
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+    return view('admin.index');
+})->name('admin');
 */
